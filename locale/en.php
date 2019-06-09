1d9ccec75d9ef4e57b29b0d18cb171c935028597
Mehdi <m.mahdi.gh1997@gmail.com>
2019-06-09 13:04:24 +0430

    Local Directory: includes all translated bot sentences
    added: en.php

---
+++ locale/en.php
@@ -0,0 +1,27 @@
+<?
+// Translate System
+define('_bot_is_updated',"🆕 The bot upgrade to the new version");
+define('_you_already_start',"❗ You have already started the robot");
+define('_send_your_link',"Please send your link");
+define('_panel_for_superadmin',"🔰Super Admin Panel🔰");
+define('_number_users',"Number of bot users is: ");
+define('_number_kutt_link',"Number of Kutt link is: ");
+define('_exit_from_update_state',"Bot was released from update state");
+define('_update_for_bot',"Bot is in a new update state");
+define('_panel_for_admin',"🔰Admin Panel🔰");
+define('_link_is_not_standard',"ℹ Please send your link with http:// or https://");
+
+define('_help_for_admin',"⁉ Admin's instruction
+
+✉ If you wanna send message to all bot user, first type your message. then touch the ✉ Send to All button, finally send your Message immediately. your message will send to all bot users.
+
+👤 For get number of bot user just touch the 👤 Users button.
+
+🔗 For get number of Kutt.it links that shorten by bot, touch 🔗 Short Links button.
+
+🔧 If you have publish a new update for the bot and you wanna bot users see a right message when they hit /start, just touch the 🔧 Update State button. To know about using this button once put the bot in this state and /start, after that touch this button again and /start.
+
+🚪  For Close Admin / Super Admin Panel, touch 🚪 Exit button.
+
+Good Luck😉
+");
