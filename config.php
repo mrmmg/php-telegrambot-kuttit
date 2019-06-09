679f778d20a1ebadc7bc70d7734633b21a6e8649
Mehdi <m.mahdi.gh1997@gmail.com>
2019-06-09 15:50:22 +0430

    update config.php

--- config.php
+++ config.php
@@ -3,21 +3,21 @@ if (!defined('test')) { exit; }
 //Database Section
 global $config;
 $config['db']['host'] = 'localhost';                // Database Host
-$config['db']['user'] = 'mrmmgir_userkuttit';      //  Database Username
-$config['db']['pass'] = 'FLiv6}50R_B.X5T*Py';     //   Database Password
-$config['db']['name'] = 'mrmmgir_kuttitbot';     //    Dababase Name
+$config['db']['user'] = 'username';      //  Database Username
+$config['db']['pass'] = 'password';     //   Database Password
+$config['db']['name'] = 'db name';     //    Dababase Name
 
 //API Section
-$config['api']['kutt'] = 'U7vD4CdWH7rLWIT8vKDSxuJa3XghM0S1vNlt5sfG';           // Kutt.it API Key
-$config['api']['telegram'] = '814358071:AAF8X4mn2O0FO6Xxt_blhYiSmMo5Gdlb3nM'; //  Telegram Bot API Key
+$config['api']['kutt'] = 'xxxx';           // Kutt.it API Key
+$config['api']['telegram'] = 'xx:xxxxx'; //  Telegram Bot API Key
 
 //Admins Section
-$config['admin']['owner'] = '242974307';  //Super Admin Telegram ID
+$config['admin']['owner'] = 'xxxxxxxxx';  //Super Admin Telegram ID
 /**
 * Admins Telegram ID | Optional
 * IF you do not want to have an admin for your robot, do not change this area ELSE Complete it!
 */
 $config['admin']['other'] = array(
-  'admin1' => '334673986',
-  'admin2' => '104886185',
+  'admin1' => 'xxxxxxxxx',
+  'admin2' => 'xxxxxxxxx',
 );
